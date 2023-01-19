4307b742e57c3e5288c9108a0472d31b0d515c61
unknown <bayuxbca@gmail.com>
2023-01-19 11:31:35 +0700

    create User API controller ( authentication )

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Exception;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function login(Request $request)
+    {
+        try {
+            
+            // Validate Request
+
+            // Find User by Email
+
+            // Generate Token
+
+            // Return Response
+            
+        } catch (Exception $e) {
+            //throw $th;
+        }
+    }
+}
