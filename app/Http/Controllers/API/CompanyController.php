b9b470c7b232f456b9101cdeecc21c387bf0d6cc
unknown <bayuxbca@gmail.com>
2023-01-25 22:04:48 +0700

    create company controller & request

--- app/Http/Controllers/API/CompanyController.php
+++ app/Http/Controllers/API/CompanyController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Helpers\ResponseFormatter;
+use App\Http\Requests\CreateCompanyRequest;
 use App\Models\Company;
 
 class CompanyController extends Controller
@@ -41,5 +42,19 @@ class CompanyController extends Controller
             $companies->paginate($limit),
             'Company Found!'
         );
+    }
+
+    public function create(CreateCompanyRequest $request)
+    {
+
+        if($request->hasFile('logo'))
+        {
+            $path = $request->file('logo')->store('public/logos');
+        }
+
+        $company = Company::create([
+            'name' => $request->name,
+            'logo' => $path,
+        ]);
     }
 }
