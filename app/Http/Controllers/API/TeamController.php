8fb680714cf680508edc7f74b3b5c6b90321f7af
unknown <bayuxbca@gmail.com>
2023-01-27 20:48:54 +0700

    initiate Team controller API & make request

---
+++ app/Http/Controllers/API/TeamController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TeamController extends Controller
+{
+    public function fetch()
+    {
+
+    }
+
+    public function create()
+    {
+        
+    }
+}
