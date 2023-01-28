26150d5f915d182c3728826f915020d7ca774f0e
unknown <bayuxbca@gmail.com>
2023-01-28 18:16:41 +0700

    update Team controller API

--- app/Http/Controllers/API/TeamController.php
+++ app/Http/Controllers/API/TeamController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
+use Exception;
+use App\Models\Team;
 use Illuminate\Http\Request;
+use App\Helpers\ResponseFormatter;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateTeamRequest;
 
 class TeamController extends Controller
 {
@@ -12,8 +16,29 @@ class TeamController extends Controller
 
     }
 
-    public function create()
+    public function create(CreateTeamRequest $request)
     {
-        
+        try {
+
+            if($request->hasFile('icon'))
+            {
+                $path = $request->file('icon')->store('public/icons');
+            }
+
+            $team = Team::create([
+                'name' => $request->name,
+                'icon' => $path
+            ]);
+
+            if(!$team)
+            {
+                throw new Exception('Team Not Found!');
+            }
+
+            return ResponseFormatter::success($team, 'Team Created');
+
+        } catch (Exception $e) {
+            return ResponseFormatter::error($e->getMessage(),500 );
+        }
     }
 }
